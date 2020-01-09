43d522cad05c4b93ae44f97a695b54553304e8b6
zhangchunhua <vera0228@sohu.com>
2020-01-09 18:01:01 +0800

    add buy ticket calendar

--- pages/user/user.vue
+++ pages/user/user.vue
@@ -63,11 +63,39 @@
 				<navigator url="register"><view class="uni-card-link">去注册</view></navigator>
 			</view>
 		</view>
+		<view class="uni-card">
+			<view class="uni-card-header uni-card-media">
+				<view class="uni-card-media-body">
+					<view>
+						<uni-calendar 
+							:insert="true"
+							:lunar="true" 
+							:start-date=departureStart
+							:end-date=departureEnd
+							@change="changeDepartureCalendar"
+							 ></uni-calendar>
+					</view>
+					<view>
+						<uni-calendar 
+							:insert="true"
+							:lunar="true" 
+							:endDate=buyEnd
+							:selected=buySelected
+							ref="buyCalendar"
+							 ></uni-calendar>
+					</view>
+				</view>
+			</view>
+		</view>
 	</view>
 </template>
 
 <script>
+	import uniCalendar from '@/components/uni-calendar/uni-calendar.vue'
 	export default {
+		components: {
+			uniCalendar
+		},
 		data() {
 			return {
 				yearlyConf: {
@@ -79,10 +107,38 @@
 				user: {},
 				// show: false,
 				list: [],
-				currentShow: -1
+				currentShow: -1,
+				//出行开始和截止日期
+				departureStart: '2019-3-2',
+				departureEnd: '2020-2-28',
+				//可购买票的开始和截止日期标记可买票的第一天
+// 				buyStart: '2020-02-05',
+				buyEnd: '2020-03-03',
+				buySelected: [],
 			}
 		},
 		methods:{
+			changeDepartureCalendar(e) {
+				// console.log(e);
+				var clickDate = new Date(e.fulldate);
+				clickDate.setDate(e.date + 29);
+				var y = clickDate.getFullYear();
+				var m = (clickDate.getMonth() + 1);
+				var buyDate = y + '-' + m + '-' + clickDate.getDate();
+				var buyCalendar = this.$refs.buyCalendar;
+				this.buySelected = [{date: buyDate, info: '买此之前票'}];
+				this.buyEnd = buyDate;
+				buyCalendar.endDate = buyDate;
+				buyCalendar.setDate(buyDate);
+				buyCalendar.monthSwitch();
+				buyCalendar.change();
+			},
+// 			openCalendar(){
+// 				this.$refs.calendar.open();
+// 			},
+			confirmCalendar(e) {
+				console.log(e);
+			},
 			trigerCollapse(index) {
 				this.currentShow = index;
 			},
