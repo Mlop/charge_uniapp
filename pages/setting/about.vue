6654372754bbda5b6ac4922483b55d8e00351911
zhangchunhua <vera0228@sohu.com>
2020-04-16 15:15:43 +0800

    add version update

--- pages/setting/about.vue
+++ pages/setting/about.vue
@@ -1,9 +1,8 @@
 <template>
 	<view class="uni-padding-wrap uni-common-pb">
 		<view class="uni-header-logo">
-			<image src="/static/template.png"></image>
 			<view class="version">
-				当前版本：{{version}}
+				当前版本：{{versionObj.version}}
 			</view>
 		</view>
 		<view class="uni-hello-text uni-common-pb">
@@ -12,12 +11,12 @@
 		<view class="uni-card" v-for="(list,index) in lists" :key="index">
 			<view class="uni-list">
 				<view class="uni-list-cell uni-collapse">
-					<view class="uni-list-cell-navigate" hover-class="uni-list-cell-hover" :class="[list.open ? 'uni-active' : '',list.pages ? 'uni-navigate-bottom' : 'uni-navigate-right']"
-					 >
+					<view class="uni-list-cell-navigate" hover-class="uni-list-cell-hover" :class="[list.open ? 'uni-active' : '',list.pages ? 'uni-navigate-bottom' : 'uni-navigate-right']">
 						{{list.name}}
+						<view :class="(list.url=='version_update' && versionObj.badge == true) ? 'version-hint' : ''" @click="goDetailPage(list)"></view>
 					</view>
 					<view class="uni-list uni-collapse" v-if="list.pages" :class="list.open ? 'uni-active' : ''">
-						<view class="uni-list-cell" hover-class="uni-list-cell-hover" v-for="(item,key) in list.pages" :key="key" @click="goDetailPage(item)">
+						<view class="uni-list-cell" hover-class="uni-list-cell-hover" v-for="(item,key) in list.pages" :key="key">
 							<view class="uni-list-cell-navigate uni-navigate-right"> {{item.name ? item.name : item}} </view>
 						</view>
 					</view>
@@ -30,21 +29,33 @@
 	export default {
 		data() {
 			return {
-				version: '1.0.0',
+				versionObj: {version: "1.0.1", badge: false},
 				lists: [{
 						name: '联系方式',
-						url: 'badge'
+						url: 'badge',
 					}, {
 						name: '平台协议',
-						url: 'number-box'
+						url: 'number-box',
 					}, {
 						name: '新版本',
-						url: 'popup'
+						url: 'version_update',
 					},
 				]
 			}
 		},
+		onLoad: function (options) {
+			var _this = this;
+			_this.request('GET', 'apk/latest', {}, function(data){
+				_this.versionObj = data;
+				_this.versionObj.badge = true;
+			});
+		},
 		methods: {
+			goDetailPage(item) {
+				uni.navigateTo({
+					url: item.url
+				});
+			}
 		}
 	}
 </script>
@@ -73,5 +84,17 @@
 	.uni-header-logo uni-image {
 		width: 79px;
 		height: 79px;
+	}
+	.version-hint {
+		width:5px;
+		height:5px;
+		margin-right: 10px;
+		border: 2px solid #DD524D;
+		text-align: center;
+		line-height: 5px;
+		font-weight: bold;
+		font-size: 24px;
+		background: #DD524D;
+		border-radius: 50%;
 	}
 </style>
---
+++ pages/setting/version_update.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		<web-view :src="download_url" :progress="false"></web-view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				download_url: '',
+			}
+		},
+		onLoad: function (options) {
+			var _this = this;
+			_this.request('GET', 'apk/latest', {}, function(data){
+				_this.download_url = data.url;
+				uni.navigateBack();
+			});
+		},
+	}
+</script>
